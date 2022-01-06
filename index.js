1da9f3b6192cce89b5157f250f2762f28d881a95
zhangmeteor@me.com <hao@17lianqin.cn>
2022-01-06 16:04:57 +0800

    update

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const shell = require('shelljs');
+
+const sleep = function(sec) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res();
+        }, sec * 1000)
+    })
+};
+
+(async () => {
+
+    for (;;) {
+        try {
+        
+            fs.appendFileSync('./aa', 'addText')
+            
+            shell.exec(`git add ./aa`);
+            // git commit --date="10 day ago" -m "Your commit message" 
+            shell.exec(`git commit --date="1000 day ago" -m "update some text"`);
+
+        } catch (error) {
+            console.error('error', error);
+        }
+    }
+})();
